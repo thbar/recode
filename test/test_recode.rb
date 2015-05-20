6437d3fd6b57ce5e003455b77a90ea794b4b6154
Thibaut Barrère <thibaut.barrere@gmail.com>
2015-05-21 00:23:09 +0200

    Start playing around with Song.xml data

--- test/test_recode.rb
+++ test/test_recode.rb
@@ -1,5 +1,6 @@
 require_relative 'helper'
 require 'recode'
+require 'nokogiri'
 
 class RecodeTest < Minitest::Test
   
@@ -21,6 +22,18 @@ class RecodeTest < Minitest::Test
     before = `unzip -l #{template} | tail -n +2 | sort`
     after = `unzip -l #{target} | tail -n +2 | sort`
     assert_equal(after, before)
+  end
+  
+  def test_song_tweaking
+    Recode.generate('test/fixtures/blank.xrns', from: template) do |dir|
+      song_file = File.join(dir, 'Song.xml')
+      doc = Nokogiri::XML(IO.read(song_file))
+      doc.search('/RenoiseSong/PatternPool/Patterns/Pattern/Tracks/PatternTrack').each do |node|
+        # note: this will crash Renoise - we must generate replacements PatternTrack here instead
+        node.remove
+      end
+      IO.write(song_file, doc.to_xml)
+    end
   end
     
 end
